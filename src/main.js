58b0d057abcebfb97e3473ea488adf17d2019460
Vicki Keeley <vicki.keeley@gmail.com>
2018-10-07 16:18:01 +0100

    add firebase, firestore and firebaseui. add api key file for translate

--- src/main.js
+++ src/main.js
@@ -4,6 +4,23 @@ import router from './router'
 import store from './store'
 import './registerServiceWorker'
 
+import firebase from "firebase/app";
+import 'firebase/firestore';
+import * as firebaseui from 'firebaseui';
+
+const config = {
+  apiKey: "AIzaSyAbIgWIKY02k19aYWM_TP4q0tdggcZ3qqM",
+  authDomain: "chatranslate-218619.firebaseapp.com",
+  databaseURL: "https://chatranslate-218619.firebaseio.com",
+  projectId: "chatranslate-218619",
+  storageBucket: "chatranslate-218619.appspot.com",
+  messagingSenderId: "666806704863"
+};
+
+firebase.initializeApp(config);
+
+console.log(firebase.app(), firebaseui);
+
 Vue.config.productionTip = false
 
 new Vue({
