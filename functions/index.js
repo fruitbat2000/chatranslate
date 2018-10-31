684ad3949d886edab9971ec4250d9a14fc2cceca
Vicki Keeley <vicki.keeley@gmail.com>
2018-10-31 11:32:38 +0000

    auto send invite email when a new invite is added to the db

--- functions/index.js
+++ functions/index.js
@@ -7,15 +7,32 @@ admin.initializeApp();
 
 exports.sendTestEmail = functions.https.onRequest((request, response) => {
 	let data = {
-	  from: 'Excited User <me@samples.mailgun.org>',
-	  to: 'vicki.keeley@gmail.com',
-	  subject: 'Hello',
-	  text: 'Testing some Mailgun awesomeness!'
+		from: 'Excited User <me@samples.mailgun.org>',
+		to: 'vicki.keeley@gmail.com',
+		subject: 'Hello',
+		text: 'Testing some Mailgun awesomeness!'
 	};
 
 	mailgun.messages().send(data, (error, body) => {
-    console.log(body)
-  });
+		console.log(body)
+	});
 
-  response.send('yay!');
+	response.send('yay!');
 });
+
+exports.sendInvite = functions.firestore
+	.document('invites/{inviteId}')
+	.onCreate((snap, context) => {
+		const invite = snap.data();
+		let data = {
+			from: 'Fluentsy <noreply@fluensy.app>',
+			to: invite.to,
+			subject: 'You have a new invite on Fluentsy',
+			text: 'Hi there!, '+invite.fromName+'('+invite.fromEmail+') wants to chat with you on Fluentsy. Use the link below to accept their invite and start a conversation...'
+		};
+
+		console.log(invite, data);
+		mailgun.messages().send(data, (error, body) => {
+			console.log(body)
+		});
+	});
