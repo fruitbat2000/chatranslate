a3698a4510260ea9b67f960a5ce19322b6d78c61
Vicki Keeley <vicki.keeley@gmail.com>
2018-11-30 15:09:51 +0000

    create and store chats in firebase

--- src/views/contacts.vue
+++ src/views/contacts.vue
@@ -16,6 +16,7 @@
 </template>
 
 <script>
+import firebase from "firebase/app";
 import userCard from '@/components/userCard'
 
 export default {
@@ -25,12 +26,47 @@ export default {
 	},
 	data() {
 		return {
-
+			db: this.$store.state.db
 		}
 	},
 	methods: {
 		startChat(contact) {
-			console.log('startChat', contact)
+			console.log('startChat - create new chat and attach to users, then redirect to the chat page', contact)
+			let chat = {
+					members: [{uid: this.$store.state.user.uid, lang: 'en'}, {uid: contact, lang: 'es'}],
+					messages: []
+				},
+				currentUserDoc = this.db.collection('users').doc(this.$store.state.user.uid),
+				contactDoc = this.db.collection('users').doc(contact)
+
+			this.db.collection('chats').add(chat)
+				.then(docRef => {
+					currentUserDoc.update({
+						chats: firebase.firestore.FieldValue.arrayUnion(docRef.id)
+					});
+
+					contactDoc.update({
+						chats: firebase.firestore.FieldValue.arrayUnion(docRef.id)
+					});
+				})
+				.catch(err => {
+					console.log(err)
+				})
+
+			// a chat would be posted to firebase in it's original language, then
+			// 1. a recipient client would be notified of the new message and then request it in the desired language?
+			// OR
+			// 2. we know the desired language(s) in advance (ie they're set at the beginning of the chat or use a default)
+			// and when the message is posted, translations are created automatically. Only the desired language is displayed
+
+			// suspect 2. will be quicker.
+
+			//	messages: [{
+			//		original: '',
+			//		language: '',
+			//		from: '',
+			//		translations: [{lang: '', text: ''},{lang: '', text: ''}]
+			//	}]
 		}
 	},
 	mounted() {
